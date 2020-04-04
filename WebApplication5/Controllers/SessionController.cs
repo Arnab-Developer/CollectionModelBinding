c739eee4ab1b9277d91dd50f413f4ca2176c9424
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-04-04 11:08:50 +0530

    Add project files.

---
+++ WebApplication5/Controllers/SessionController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            var s = new Session()
+            {
+                Id = 1,
+                Name = "session1",
+                Students = new List<Student>()
+                {
+                    new Student()
+                    {
+                        Id = 1,
+                        Name = "student1"
+                    },
+                    new Student()
+                    {
+                        Id = 2,
+                        Name = "student2"
+                    }
+                }
+            };
+            return View(s);
+        }
+
+        public IActionResult Create(Session s)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication5/Models/SchoolModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
